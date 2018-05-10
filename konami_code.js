32f20972aff503ec70d93ec07205bfe9ccb9a683
Allyson Wesman <allysonwesman@gmail.com>
2018-05-10 14:49:45 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+function init() {
+  let index = 0;
+
+  document.body.addEventListener('keydown', keyDownHandler)
+
+  function keyDownHandler(event) {
+    const key = parseInt(event.which || event.detail);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert('correct!')
+        index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
+  }
 }
